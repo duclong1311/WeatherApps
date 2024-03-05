3ac8fa88e63774499e7a93018ee18c5dc657e609
duclong1311 <duclong1311.work@gmail.com>
2024-03-05 15:51:15 +0700

    Adding MVVM
    adding view, model and viewmodel

---
+++ WeatherApp/ViewModels/WeatherViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.ViewModels
+{
+    public class WeatherViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
