91bf2ee4d047ce4c5e52265c1f44078ade163049
duclong1311 <duclong1311.work@gmail.com>
2024-03-05 15:59:53 +0700

    INotyfypropertyChanged
    little update

--- WeatherApp/ViewModels/WeatherViewModel.cs
+++ WeatherApp/ViewModels/WeatherViewModel.cs
@@ -9,11 +9,26 @@ namespace WeatherApp.ViewModels
 {
     public class WeatherViewModel : INotifyPropertyChanged
     {
+        private string query;
+        public string Query
+        {
+            get
+            {
+                return query;
+            }
+            set
+            {
+                query = value;
+                OnPropertyChanged("Query");
+            }
+        }
         public event PropertyChangedEventHandler? PropertyChanged;
 
         private void OnPropertyChanged(string propertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+
     }
+
 }
